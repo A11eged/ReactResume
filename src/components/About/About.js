cfbd6d3fcf86835b09efeb663339cf3fe37a3713
HungryMidas <minhhtran18@gmail.com>
2023-10-20 03:02:57 -0400

    Fixed descriptions

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import styles from './About.module.css';
 import AnimatedLettersHeader from '../UX/AnimatedLettersHeader';
-import { Description } from '@mui/icons-material';
+import Description from '../UI/Description';
 
 const About = () => {
   return (
     <div className={styles.about} id="about">
       <AnimatedLettersHeader text={[{ type: 'heading1', text: 'About Me' }]}/>
       <Description text={
-        ``
+        `
+        Thanks for visiting my website! My name is Minh Tran and I currently live in Boston. 
+        I love coding, working with data, and using the two to create visually pleasing webapps. 
+        `
+      }/>
+      <Description text={
+        `
+        I'm a recently graduated full stack web developer with a passion for creating beautiful user experiences. 
+        I have a background in economics and a passion for building web applications and working with data. 
+        `
+      }/>
+      <Description text={
+        `
+        On this site, you can browse through my portfolio of projects and studies that I have done. Hope you enjoy!
+        `
       }/>
     </div>
   );
 };
-/* I am a full stack web developer with a passion for creating beautiful
-user experiences. I have a background in finance and a passion for
-building web applications. I am currently looking for a position in the
-web development industry. */
+
 export default About;

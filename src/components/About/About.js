f2cc41855c8c3f0d665e60cc284e83c5b2065869
HungryMidas <minhhtran18@gmail.com>
2023-10-22 23:47:11 -0400

    broke up sentences

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,23 +7,27 @@ const About = () => {
   return (
     <div className={styles.about} id="about">
       <AnimatedLettersHeader text={[{ type: 'heading1', text: 'About Me' }]}/>
-      <Description text={
-        `
-        Thanks for visiting my website! My name is Minh Tran and I currently live in Boston. 
-        I love coding, working with data, and using the two to create visually pleasing webapps. 
-        `
-      }/>
-      <Description text={
-        `
-        I'm a recently graduated full stack web developer with a passion for creating beautiful user experiences. 
-        I have a background in economics and a passion for building web applications and working with data. 
-        `
-      }/>
-      <Description text={
-        `
-        On this site, you can browse through my portfolio of projects and studies that I have done. Hope you enjoy!
-        `
-      }/>
+      <div className={styles.descriptionContainer}>
+        <Description text={
+          `
+          Thanks for visiting my website! My name is Minh Tran and I currently live in Boston. 
+          I love coding, working with data, and using the two to create visually pleasing webapps. 
+          `
+        }/>
+        <div className={styles.br}/>
+        <Description text={
+          `
+          I'm a recently graduated full stack web developer with a passion for creating beautiful user experiences. 
+          I have a background in economics and a passion for building web applications and working with data. 
+          `
+        }/>
+        <div className={styles.br}/>
+        <Description text={
+          `
+          On this site, you can browse through my portfolio of projects and studies that I have done. Hope you enjoy!
+          `
+        }/>
+      </div>
     </div>
   );
 };
